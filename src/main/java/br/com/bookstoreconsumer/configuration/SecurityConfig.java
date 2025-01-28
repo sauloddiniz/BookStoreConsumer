32d21679aff236c66d0ca93ec8cf3b20f4530599
Saulo Dias Diniz <sauloddiniz@gmail.com>
2025-01-28 05:45:05 -0300

    Update security, JWT authentication, and API documentation
    Removed `UserController` in favor of JWT-based authentication. Enhanced `SecurityConfig` to include OAuth2 login with JWT generation and updated redirects to Swagger UI. Refined OpenAPI documentation for books and authors.

--- src/main/java/br/com/bookstoreconsumer/configuration/SecurityConfig.java
+++ src/main/java/br/com/bookstoreconsumer/configuration/SecurityConfig.java
@@ -1,13 +1,17 @@
 package br.com.bookstoreconsumer.configuration;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.http.Cookie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Date;
+
 
 @Configuration
 public class SecurityConfig {
@@ -21,27 +25,34 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS
                         ))
                 .authorizeHttpRequests(authorize -> {
-                            authorize.requestMatchers("/actuator/**").permitAll();
+                            authorize.requestMatchers(
+                                    "/swagger-ui/**",
+                                    "/v3/api-docs/**",
+                                    "/api-docs.yaml"
+                            ).permitAll();
                             authorize.anyRequest().authenticated();
                         }
                 )
-                .oauth2Login(oAuth2Configurer ->
-                        oAuth2Configurer.defaultSuccessUrl("/home", true));
-
+                .oauth2Login(
+                        oAuth2Configurer ->
+                                oAuth2Configurer
+                                        .successHandler((request, response, authentication) -> {
+                                            OidcUser principal = (OidcUser) authentication.getPrincipal();
+                                            String email = principal.getEmail();
+                                            String name = principal.getGivenName();
+                                            String secretKey = "secreta-chave-de-seguranca";
+                                            String jwtToken = JWT.create()
+                                                    .withSubject(email)
+                                                    .withClaim("name", name)
+                                                    .withClaim("email", email)
+                                                    .withIssuedAt(new Date())
+                                                    .withExpiresAt(new Date(System.currentTimeMillis() + 3600 * 1000))
+                                                    .sign(Algorithm.HMAC256(secretKey));
+                                            response.addHeader("Authorization", "Bearer " + jwtToken);
+                                            response.addCookie(new Cookie("jwtToken", jwtToken));
+                                            response.sendRedirect("/swagger-ui/index.html");
+                                        })
+                );
         return http.build();
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-
 }
--- src/main/java/br/com/bookstoreconsumer/configuration/SwaggerConfig.java
+++ src/main/java/br/com/bookstoreconsumer/configuration/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springdoc.core.configuration.SpringDocUIConfiguration;
 import org.springdoc.core.properties.SpringDocConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,22 +14,27 @@ import java.util.Optional;
 @Configuration
 public class SwaggerConfig {
 
+
     @Bean
+    @Qualifier("springDocConfiguration")
     SpringDocConfiguration springDocConfiguration() {
         return new SpringDocConfiguration();
     }
 
     @Bean
+    @Qualifier("springDocConfigProperties")
     SpringDocConfigProperties springDocConfigProperties() {
         return new SpringDocConfigProperties();
     }
 
     @Bean
+    @Qualifier("objectMapperProvider")
     ObjectMapperProvider objectMapperProvider(SpringDocConfigProperties springDocConfigProperties) {
         return new ObjectMapperProvider(springDocConfigProperties);
     }
 
     @Bean
+    @Qualifier("springDocUIConfiguration")
     SpringDocUIConfiguration springDocUiConfiguration(Optional<SwaggerUiConfigProperties> optionalSwaggerUiConfigProperties) {
         return new SpringDocUIConfiguration(optionalSwaggerUiConfigProperties);
     }
